6daf5ebce600e69b847e963a7a09701a1e2e54ad
NazioLT <94365544+NazioLT@users.noreply.github.com>
2022-09-06 14:46:26 +0200

    Vector Bounding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,5 +12,6 @@
 #### Math Library
 
 * Vector2 Angle calculs. ([32fe54a](https://github.com/NazioLT/NZCore/commit/f29f2f141f031dd9b24ee135425399ea1b110ce2))
-* Epsilon Value.
-* Average zero method.
+* Epsilon Value. ([630829d](https://github.com/NazioLT/NZCore/commit/630829d3267bf617d7b0e349b061f5d9e2f008cb))
+* Average zero method. ([630829d](https://github.com/NazioLT/NZCore/commit/630829d3267bf617d7b0e349b061f5d9e2f008cb))
+* Check if Vector Axis values is beetwen 2 others. 
--- Runtime/Libraries/NMath.cs
+++ Runtime/Libraries/NMath.cs
@@ -8,6 +8,60 @@ namespace Nazio_LT.Core
 
         public static bool AverageZero(float _value) => _value < EPSILON && _value > -EPSILON;
 
+        #region Vector Bounding
+
+        public static bool InBoundsInclusive(Vector2Int _value, Vector2Int _bounds) => InBoundsInclusive(_value, Vector2Int.zero, _bounds);
+        public static bool InBoundsInclusive(Vector2Int _value, Vector2Int _minBounds, Vector2Int _maxBounds) => InBoundsInclusive((Vector2)_value, (Vector2)_minBounds, (Vector2)_maxBounds);
+
+        public static bool InBoundsInclusive(Vector3Int _value, Vector3Int _bounds) => InBoundsInclusive(_value, Vector3Int.zero, _bounds);
+        public static bool InBoundsInclusive(Vector3Int _value, Vector3Int _minBounds, Vector3Int _maxBounds) => InBoundsInclusive((Vector3)_value, (Vector3)_minBounds, (Vector3)_maxBounds);
+
+        public static bool InBoundsInclusive(Vector2 _value, Vector2 _bounds) => InBoundsInclusive(_value, Vector2.zero, _bounds);
+        public static bool InBoundsInclusive(Vector2 _value, Vector2 _minBounds, Vector2 _maxBounds) => InBoundsInclusive((Vector3)_value, (Vector3)_minBounds, (Vector3)_maxBounds);
+
+        public static bool InBoundsInclusive(Vector3 _value, Vector3 _bounds) => InBoundsInclusive(_value, Vector3.zero, _bounds);
+        public static bool InBoundsInclusive(Vector3 _value, Vector3 _minBounds, Vector3 _maxBounds)
+        {
+            if (!CheckVectorAxis(_value, _minBounds, _maxBounds, VectorAxis.X)) return false;
+
+            if (!CheckVectorAxis(_value, _minBounds, _maxBounds, VectorAxis.Y)) return false;
+
+            if (!CheckVectorAxis(_value, _minBounds, _maxBounds, VectorAxis.Z)) return false;
+
+            return true;
+        }
+
+        private static bool CheckVectorAxis(Vector3 _value, Vector3 _minBounds, Vector3 _maxBounds, VectorAxis _axis)
+        {
+            if (!VectorAxisInBounds(_value, _minBounds, _maxBounds, _axis))
+            {
+                Debug.LogErrorFormat($"The X value of {_value} isn't in bounds ({_minBounds}:{_maxBounds})");
+                return false;
+            }
+
+            return true;
+        }
+
+        public enum VectorAxis { X, Y, Z }
+
+        public static bool VectorAxisInBounds(Vector2 _value, Vector2 _minBounds, Vector2 _maxBounds, VectorAxis _axis) => VectorAxisInBounds((Vector3)_value, (Vector3)_minBounds, (Vector3)_maxBounds, _axis);
+
+        public static bool VectorAxisInBounds(Vector3 _value, Vector3 _minBounds, Vector3 _maxBounds, VectorAxis _axis)
+        {
+            switch (_axis)
+            {
+                case VectorAxis.X:
+                    return (_value.x >= _minBounds.x && _value.x <= _maxBounds.x);
+
+                case VectorAxis.Y:
+                    return (_value.y >= _minBounds.y && _value.y <= _maxBounds.y);
+            }
+
+            return (_value.z >= _minBounds.z && _value.z <= _maxBounds.z);
+        }
+
+        #endregion
+
         public static Vector2 Vector2PerAxis(bool _firstX, float _value1, float _value2) => new Vector2(_firstX ? _value1 : _value2, _firstX ? _value2 : _value1);
 
         #region Angles
