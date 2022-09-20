f60ab1dc6894555bf036b584d3be0cc8d2a31ae6
Nazio_LT <zoupeurletroll@gmail.com>
2022-09-20 12:08:34 +0200

    Bezier Derivative

--- Runtime/Libraries/BezierCurve.cs
+++ Runtime/Libraries/BezierCurve.cs
@@ -19,17 +19,29 @@ namespace Nazio_LT.Tools.Core
 
         public static Vector3 BezierLerp(Vector3 _p1, Vector3 _p2, Vector3 _p3, Vector3 _p4, float _t)
         {
-            float _tCube = Mathf.Pow(_t,3f);
+            float _tCube = Mathf.Pow(_t, 3f);
             float _tSquare = _t * _t;
 
-            Vector3 _p1P = (-_tCube + 3 * _tSquare - 3 * _t + 1) * _p1;
-            Vector3 _p2P = (3 * _tCube - 6 * _tSquare + 3 * _t) * _p2;
-            Vector3 _p3P = (-3 * _tCube + 3 * _tSquare) * _p3;
+            Vector3 _p1P = (-_tCube + 3f * _tSquare - 3f * _t + 1f) * _p1;
+            Vector3 _p2P = (3f * _tCube - 6f * _tSquare + 3f * _t) * _p2;
+            Vector3 _p3P = (-3f * _tCube + 3f * _tSquare) * _p3;
             Vector3 _p4P = _tCube * _p4;
 
             return _p1P + _p2P + _p3P + _p4P;
         }
 
         public static Vector3 BezierLerp(NHandle _hanlde1, NHandle _handle2, float _t) => BezierLerp(_hanlde1.point, _hanlde1.forwardHelper, _handle2.backHelper, _handle2.point, _t);
+
+        public static Vector3 BezierDerivative(Vector3 _p1, Vector3 _p2, Vector3 _p3, Vector3 _p4, float _t)
+        {
+            float _tSquare = _t * _t;
+
+            Vector3 _p1P = (-3 * _tSquare + 6 * _t - 3) * _p1;
+            Vector3 _p2P = (9 * _tSquare - 12 * _t + 3) * _p2;
+            Vector3 _p3P = (-9f * _tSquare + 6 * _t) * _p3;
+            Vector3 _p4P = 3f * _tSquare * _p4;
+
+            return _p1P + _p2P + _p3P + _p4P;
+        }
     }
 }
