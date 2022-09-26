75edd8e632f0799fb754851cf6b165c8329142e4
Nazio_LT <zoupeurletroll@gmail.com>
2022-09-26 21:41:30 +0200

    Mesh along spline 2

--- Runtime/System/Splines/NCurveBehaviour.cs
+++ Runtime/System/Splines/NCurveBehaviour.cs
@@ -13,8 +13,6 @@ namespace Nazio_LT.Tools.Core
         [SerializeField] public NCurve curve;
         [SerializeField] private MeshFilter meshFilter;
 
-        private Mesh savedMesh;
-
         public void Gen()
         {
             CurveMesh _mesh = CurveMesh.Factory(meshType, curve);
@@ -25,32 +23,50 @@ namespace Nazio_LT.Tools.Core
         public void GenByMesh()
         {
             Bounds _meshBounds = meshToDeform.bounds;
-            Vector3 _origin = transform.TransformPoint(new Vector3(_meshBounds.center.x, _meshBounds.min.y, _meshBounds.min.z));
-            Vector3 _end = transform.TransformPoint(new Vector3(_meshBounds.center.x, _meshBounds.min.y, _meshBounds.max.z));
+            Vector3 _meshOrigin = transform.TransformPoint(new Vector3(_meshBounds.center.x, _meshBounds.min.y, _meshBounds.min.z));
+            Vector3 _meshEnd = transform.TransformPoint(new Vector3(_meshBounds.center.x, _meshBounds.min.y, _meshBounds.max.z));
 
-            Vector3[] _vertices = meshToDeform.vertices;
-            float _zDst = Mathf.Abs(_end.z - _origin.z);
-            for (var i = 0; i < _vertices.Length; i++)
+            float _zDst = Mathf.Abs(_meshEnd.z - _meshOrigin.z);
+            int _objNumber = (int)(curve.curveLength / _zDst);
+
+            float _tPart = 1f / (float)_objNumber;
+            float _startT = 0f;
+            for (var j = 0; j < _objNumber; j++)
             {
-                _vertices[i] = DeformVertex(_vertices[i], _origin, _end, _zDst);
+                CreateChildMesh(_meshOrigin, _meshEnd, _zDst, _startT, _tPart);
+                _startT += _tPart;
             }
-
-            savedMesh = new Mesh();
-            savedMesh.vertices = _vertices;
-            savedMesh.uv = meshToDeform.uv;
-            savedMesh.triangles = meshToDeform.triangles;
-            savedMesh.RecalculateNormals();
-
-            meshFilter.mesh = savedMesh;
         }
 
-        private Vector3 DeformVertex(Vector3 _vertex, Vector3 _start, Vector3 _end, float _zDst)
+        private void CreateChildMesh(Vector3 _origin, Vector3 _end, float _zDst, float _startT, float _tPart)
+        {
+            GameObject _sub = new GameObject("Spline Sub Mesh");
+            _sub.transform.parent = transform;
+            _sub.AddComponent<MeshRenderer>();
+            MeshFilter _mf = _sub.AddComponent<MeshFilter>();
+
+            Vector3[] _vertices = meshToDeform.vertices;
+            Mesh objMesh = new Mesh();
+            for (var i = 0; i < _vertices.Length; i++)
+            {
+                _vertices[i] = DeformVertex(_vertices[i], _origin, _end, _zDst, _startT, _tPart);
+            }
+
+            objMesh.vertices = _vertices;
+            objMesh.uv = meshToDeform.uv;
+            objMesh.triangles = meshToDeform.triangles;
+            objMesh.RecalculateNormals();
+
+            _mf.mesh = objMesh;
+        }
+
+        private Vector3 DeformVertex(Vector3 _vertex, Vector3 _start, Vector3 _end, float _zDst, float _startT, float _tPart)
         {
             float _vDst = Mathf.Abs(_vertex.z - _start.z);
             float _localT = _vDst / _zDst;
+            float _curveT = _startT + (_localT * _tPart);
             Vector3 _refPoint = Vector3.Lerp(_start, _end, _localT);
-            Vector3 _new = _vertex - _refPoint + curve.ComputePoint(_localT);
-            print(_new);
+            Vector3 _new = _vertex - _refPoint + curve.ComputePoint(_curveT);
 
             return _new;
         }
